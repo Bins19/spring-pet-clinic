cd7ed10095b06b59219084bc2e0bfa63275f66f9
Bins19 <badianebina@gmail.com>
2023-02-09 22:55:16 +0100

    Create Pet Type Map Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springpetclinic.services;
+
+import guru.springframework.springpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
