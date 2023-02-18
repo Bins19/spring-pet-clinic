294e9934b95c21bf4d3f46335308434dae2b8c81
Bins19 <badianebina@gmail.com>
2023-02-18 18:57:18 +0100

    Add Map Based Visit Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springpetclinic.services;
+
+import guru.springframework.springpetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
